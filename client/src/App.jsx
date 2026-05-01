052a0a8b9f7ef37b3dac74779a582c55377c5fc4
SHIVAIN2405 <shivainkoul788@gmail.com>
2026-05-01 15:23:25 +0530

    fix cors for vercel domains

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Plots from "./pages/Plots";
-import PlotDetails from "./pages/PlotDetails";
-import Login from "./pages/Login";
-import AddPlot from "./pages/AddPlot";
-import EditPlot from "./pages/EditPlot";
-import Contact from "./pages/Contact";
+const express = require("express");
+const cors = require("cors");
+const testRoutes = require("./routes/testRoutes");
+const plotRoutes = require("./routes/plotRoutes");
+const authRoutes = require("./routes/authRoutes");
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Plots />} />
-        <Route path="/plots" element={<Plots />} />
-        <Route path="/plots/:id" element={<PlotDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/add-plot"
-          element={
-            <ProtectedRoute>
-              <AddPlot />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit-plot/:id"
-          element={
-            <ProtectedRoute>
-              <EditPlot />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </>
-  );
-}
+const app = express();
 
-export default App;
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://realestate-rho-gray.vercel.app",
+      "https://realestate-git-main-varshithreddygujjullas-projects.vercel.app",
+      "https://realestate-mbxtnszoa-varshithreddygujjullas-projects.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/test", testRoutes);
+app.use("/api/plots", plotRoutes);
+
+module.exports = app;
